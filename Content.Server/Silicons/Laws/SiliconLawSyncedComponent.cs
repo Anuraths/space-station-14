9b0088d971afc5b37b45f0d6073c490d9c96d468
Anuraths <192384569+Anuraths@users.noreply.github.com>
2025-02-08 22:28:07 +0000

    Basic lawsync

---
+++ Content.Server/Silicons/Laws/SiliconLawSyncedComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace Content.Server.Silicons.Laws;
+
+[RegisterComponent]
+public sealed partial class SiliconLawSyncedComponent : Component
+{
+    [ViewVariables]
+    public EntityUid? Syncer;
+}
